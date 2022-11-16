8de57471296bc8de8bd1f3a3dae89e81f39bb940
Miriam Cortes <miriamcortes.06@gmail.com>
2022-11-16 17:47:35 -0600

    final changes

---
+++ promise.js
@@ -0,0 +1,12 @@
+/* const promise = new Promise((resolve, reject) => {
+  const miNumero = 10
+
+  if (miNumero > 10) {
+    resolve('si es mayor')
+  } else {
+    (error reject('no es mayor')) 
+  }
+})
+console.log(promise)
+promise.then(console.log).catch(console.log)
++/
--- readFileMd.js
+++ readFileMd.js
@@ -1,49 +1,57 @@
-const path = require('node:path');
-const fs = require('node:fs');
-const filePath = './README.md';
-const { extractorLinks } = require('./extractorLinks.js');
-const { validate } = require('./validateLinks.js');
+/* eslint-disable prefer-promise-reject-errors */
+const path = require('node:path')
+const fs = require('node:fs')
+// const filePath = ('./README.md')
+const filePath = process.argv[2]
+const { extractorLinks } = require('./extractorLinks.js')
+const { validate } = require('./validateLinks.js')
 
-
+// Función mdLinks
 const readFileMd = (filePath, options) => {
-  //console.log(filePath);
-//return new promisse función flecha que ejecutará tdo lo que está dentro de readfileMd y como, primer parametro resolve
-
-  if (path.isAbsolute(filePath) === false) {
-    // aca debo convertir a absoluta
-    filePath = path.resolve(filePath);
-  }
-  if (fs.existsSync(filePath)) {
-    let checkPath = fs.statSync(filePath);
-    //early return
-    if (checkPath.isDirectory()) {
-      //console.log(TypeError);
+  // console.log(filePath);
+  const newPromise = new Promise((resolve, reject) => {
+    if (path.isAbsolute(filePath) === false) {
+      // console.log (filePath);
+      filePath = path.resolve(filePath)
     }
-    //else if(checkPath.isFile()){
-    let otroPath = path.extname(filePath);
-    //console.log(otroPath);
-    if (otroPath != ".md") {
-      //console.log(TypeError);
-    }
-
-    fs.readFile(filePath, 'utf-8', (err, data) => {
-      if (err) {
-      //  console.log('error: ', err);
-      } else {
-        validate(extractorLinks(data)).then(console.log);
-        
-        //en un futuro esto cambia 
+    if (fs.existsSync(filePath)) {
+      const checkPath = fs.statSync(filePath)
+      // early return
+      if (checkPath.isDirectory()) {
+        reject('Es una carpeta, por favor ingrese un archivo')
       }
-    });
+      const otroPath = path.extname(filePath)
+      if (otroPath !== '.md') {
+        reject('No es  md')
+      } else {
+        // eslint-disable-next-line n/handle-callback-err
+        fs.readFile(filePath, 'utf-8', (error, data) => {
+          if (options.validate) {
+            resolve(validate(extractorLinks(data)))
+          } else {
+            resolve(extractorLinks(data))
+          }
+        })
+      }
+    } else {
+      reject('El archivo NO EXISTE!')
+    }
+  })
+  return newPromise
+}
+readFileMd(filePath, { validate: true }).then((result) => {
+  console.log(result)
+}).catch((error) => {
+  console.log(error)
+})
 
-  } else {
-    console.log("El archivo NO EXISTE!");
-  }
-  // Function to get current filenames
-  //in directory with specific extension
-
-
-
-};
- readFileMd(filePath)
-
+// titulo y filename
+// resuelva la promesa con los resultados, (se tiene que resolver la promesa) falta resolverla conlos datos necesarios
+// solo se esta resolviendo ruta falta conjuntarvalidate (que es el que valida los links)
+// se tiene que resolver en una promesa, tiene que retornar una promesa la función (return reject)
+// ver donde insertar las promesas
+// titulo del enlace +
+// nombre del archivo +
+// quitar console.log ./readme.md .md
+// split y array
+// metodo de array
